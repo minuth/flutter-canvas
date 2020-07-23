e2d2b6ae48b34901b48f2acf3d5f86dc029d5006
minuth <minuthprom321@gmail.com>
2020-07-23 21:32:42 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_canvas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_canvas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Canvas',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MainHomePage(title: 'Flutter Canvas'),
+    );
+  }
+}
+
+class MainHomePage extends StatefulWidget {
+  MainHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MainHomePageState createState() => _MainHomePageState();
+}
+
+class _MainHomePageState extends State<MainHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
